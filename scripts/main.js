7b7be4f6d5441b81e8477a225311ae7be5535db5
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-24 15:36:08 -0400

    First few complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,66 @@
-console.log('The Iron Yard Rocks');
+/* 
+.on()
+Attach an event handler function for one or more events 
+to the selected elements.
+
+.on( events [, selector ] [, data ], handler )
+
+
+
+//Display a paragraph's text in an alert when clicked:
+$( "p" ).on( "click", function() {
+  alert( $( this ).text() );
+});
+
+//Pass data to event handler, which is specified by name:
+function myHandler( event ) {
+  alert( event.data.foo );
+}
+$( "p" ).on( "click", { foo: "bar" }, myHandler );
+*/
+
+// $( "p" ).on( "myCustomEvent", function( event, myName ) {
+//   $( this ).text( myName + ", hi there!" );
+//   $( "span" )
+//     .stop()
+//     .css( "opacity", 1 )
+//     .text( "myName = " + myName )
+//     .fadeIn( 30 )
+//     .fadeOut( 1000 );
+// });
+// $( "button" ).click(function () {
+//   $( "p" ).trigger( "myCustomEvent", [ "John" ] );
+// });
+
+
+//Combined .on() with .hide() method via click event
+  $("#gainsboro").on("click", function(){
+    $(this).hide();
+  });
+
+//Combined .on() with .hide() method via dblclick event
+  $("#cornflowerBlue").on("dblclick", function(){
+    $(this).hide();
+  });
+
+  //.hover() event
+  $("#cornsilk").hover( function(){
+    $( this ).fadeOut( 200 );
+    $( this ).fadeIn( 200 );
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
